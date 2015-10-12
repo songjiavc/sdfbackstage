64bf61958dd1cc70fd425e8f6cae5563ab31ca3f
banna <bann@sdfcp.com>
2015-10-12 08:17:00 +0800

    结果返回bean

---
+++ parentproject/usermgr/src/main/java/com/sdf/manager/common/bean/ResultBean.java
@@ -0,0 +1,35 @@
+package com.sdf.manager.common.bean;
+
+
+/**
+ * 
+  * @ClassName: resultBean 
+  * @Description: TODO(返回数据bean) 
+  * @author bann@sdfcp.com
+  * @date 2015年10月9日 下午4:46:57 
+  *
+ */
+public class ResultBean 
+{
+	private String message;//返回提示信息
+	
+	private String status;//返回状态（success of fail）
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
