6a9517abd39b6ab2aff18a6f6cc8bf31768ad979
songj <songj@sdfcp.com>
2015-10-19 10:15:14 +0800

    补充角色实体

---
+++ parentproject/usermgr/src/main/java/com/sdf/manager/user/entity/Role.java
@@ -0,0 +1,105 @@
+package com.sdf.manager.user.entity;
+
+import java.io.Serializable;
+import java.util.HashSet;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.mapping.Set;
+import org.springframework.data.annotation.Transient;
+
+/** 
+  * @ClassName: Student 
+  * @Description: 获取
+  * @author songj@sdfcp.com
+  * @date 2015年9月23日 下午5:27:11 
+  *  
+  */
+@Entity
+@Table(name="T_SDF_ROLES")
+public class Role extends BaseEntiry implements Serializable 
+{
+	
+	
+	/** 
+	  * @Fields serialVersionUID : TODO(用一句话描述这个变量表示什么) 
+	  */ 
+	private static final long serialVersionUID = 7692132908491601627L;
+
+	@Id
+	@Column(name="ID", nullable=false, length=45)
+	@GenericGenerator(name="idGenerator", strategy="uuid")//uuid由机器生成的主键
+	@GeneratedValue(generator="idGenerator")	
+	private String id;
+	
+	@Column(name="CODE",length=45)
+	private String code;
+	
+	@Column(name="NAME",length=45)
+	private String name;
+	
+	@Column(name="PARENT_ROLE",length=45)
+	private String parentRole;
+	
+	@Transient
+	private Set users;
+	
+    //@JoinTable描述了多对多关系的数据表关系。name属性指定中间表名称，joinColumns定义中间表与Teacher表的外键关系。
+    //中间表Teacher_Student的Teacher_ID列是Teacher表的主键列对应的外键列，inverseJoinColumns属性定义了中间表与另外一端(Student)的外键关系。
+    @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JoinTable(name = "RELA_SDF_USER_ROLE", 
+            joinColumns = {  @JoinColumn(name = "ROLE_ID", referencedColumnName = "roleId")  }, 
+            inverseJoinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "userId") })
+    public Set getUsers() {
+        return users;
+    }
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getParentRole() {
+		return parentRole;
+	}
+
+	public void setParentRole(String parentRole) {
+		this.parentRole = parentRole;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+
+	
+}
