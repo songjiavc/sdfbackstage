fddde0b2ce0f11b4d5b81b0ac4031a728e2076a8
banna <bann@sdfcp.com>
2015-10-21 13:25:28 +0800

    添加根据id查询role实体的注解

--- parentproject/usermgr/src/main/java/com/sdf/manager/user/repository/RoleRepository.java
+++ parentproject/usermgr/src/main/java/com/sdf/manager/user/repository/RoleRepository.java
@@ -1,5 +1,7 @@
 package com.sdf.manager.user.repository;
 
+import org.springframework.data.jpa.repository.Query;
+
 import com.sdf.manager.common.repository.GenericRepository;
 import com.sdf.manager.user.entity.Role;
 
@@ -11,5 +13,6 @@ public interface RoleRepository extends GenericRepository<Role,String>
 	* @author bann@sdfcp.com
 	* @date 2015年10月19日 下午2:04:56
 	 */
+	@Query("select u from Role u where u.isDeleted ='1' and u.id =?1")
 	public Role getRoleById(String id);
 }
