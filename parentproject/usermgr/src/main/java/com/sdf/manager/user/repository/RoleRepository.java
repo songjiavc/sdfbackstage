b2f9f739aa44d0818ee94d7f7bbc672602a60eec
banna <bann@sdfcp.com>
2015-10-21 13:24:01 +0800

    1.角色模块dao层

---
+++ parentproject/usermgr/src/main/java/com/sdf/manager/user/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.sdf.manager.user.repository;
+
+import com.sdf.manager.common.repository.GenericRepository;
+import com.sdf.manager.user.entity.Role;
+
+public interface RoleRepository extends GenericRepository<Role,String>
+{
+	/**
+	 * 
+	* @Description: TODO(根据id获取角色详情) 
+	* @author bann@sdfcp.com
+	* @date 2015年10月19日 下午2:04:56
+	 */
+	public Role getRoleById(String id);
+}
