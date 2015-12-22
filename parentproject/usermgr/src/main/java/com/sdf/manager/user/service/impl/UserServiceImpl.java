0199dfd80897a856e260b90162a83c1e1a0351f2
songj <songj@sdfcp.com>
2015-12-22 12:37:11 +0800

    修改combobox为readonly

--- parentproject/usermgr/src/main/java/com/sdf/manager/user/service/impl/UserServiceImpl.java
+++ parentproject/usermgr/src/main/java/com/sdf/manager/user/service/impl/UserServiceImpl.java
@@ -309,7 +309,7 @@ public class UserServiceImpl implements UserService {
 	
 	public QueryResult<User> getAgentList(Class<User> entityClass, String whereJpql, Object[] queryParams,
 			LinkedHashMap<String, String> orderby, Pageable pageable) {
-			String sql = "SELECT a.* FROM echart3.T_SDF_USERS a,RELA_SDF_USER_ROLE b,T_SDF_ROLES  c "
+			String sql = "SELECT a.* FROM T_SDF_USERS a,RELA_SDF_USER_ROLE b,T_SDF_ROLES  c "
 				+ "where a.id = b.user_id "
 				+ "and   b.role_id = c.id "
 				+ "and   c.code='"+Constants.ROLE_SCDL_CODE+"' and " + whereJpql;
